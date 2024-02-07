22ad3efc6740ad17706d1b72e007e8282b4e89e3
caio-m-sanches <145612983+caio-m-sanches@users.noreply.github.com>
2024-02-07 15:54:21 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+from qrcode.image.styledpil import StyledPilImage
+import qrcode.image.styles.colormasks as masks
+import qrcode.image.svg
+import streamlit as st
+import qrcode
+
+from utils import hex_to_rgb
+from utils import setup_qrcode
+
+from config import mods_dict
+from config import mods_dict
+
+
+if not 'img' in st.session_state:
+    st.session_state.img = ''
+
+st.header('QRCode Generator', divider='rainbow')
+
+c1,c2 = st.columns(gap='medium', spec=2)
+
+with c1:    
+
+    text = st.text_input("QR Code suporta até no máximo 4 mil caracteres.", key = 'content')
+
+    c11, c12 = st.columns(2)
+
+    with c11:
+        color = st.color_picker("Escolha uma cor", "#000000", key='color_picker')
+    with c12:
+
+        st.selectbox(
+            "Selecine o formato", ['png', 'svg'], 
+            key = 'format', 
+        )
+        
+        format = st.session_state.format        
+
+    c13, c14 = st.columns(2)
+
+    with c13:
+
+        if format == 'svg':
+            
+            st.selectbox(
+                "Selecine a resolução", ['256 x 256', '512 x 512', '1024 x 1024'],
+                key = f'resolution_{format}',
+                disabled= True,
+            )
+        
+        else:
+
+            st.selectbox(
+                "Selecine a resolução", ['256 x 256', '512 x 512', '1024 x 1024'],
+                key = f'resolution_{format}',
+            )
+
+    with c14:
+
+        modStyle = st.selectbox(
+            "Selecione o tipo de traço", 
+            options= list(mods_dict[format].keys()),
+            key = 'module_style', 
+        )
+
+    c15, c16, c17 = st.columns(3)
+
+    with c16:
+        with st.empty():    
+            if st.button("I'm ready!", type = 'primary'):
+                
+                resolution = st.session_state[f'resolution_{format}']
+                setup_qrcode(text, mods_dict[format], format, color, resolution, modStyle)
+                
+with c2:
+
+    qr = qrcode.QRCode(
+        box_size=8,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        mask_pattern = 0,
+        )
+    
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(
+            image_factory=StyledPilImage, 
+            color_mask=masks.SolidFillColorMask(
+                back_color =(255, 255, 255),
+                front_color = hex_to_rgb(color),
+            ),
+            module_drawer = mods_dict['png'][modStyle],
+        )
+
+    img_st = img.convert("RGB").resize((300,300))   
+    st.image(img_st)
+
+
